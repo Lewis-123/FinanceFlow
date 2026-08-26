37b160311b3367f25d91a0427613188eba5ecb57
Lewis-123 <mucherulewis@gmail.com>
2026-08-26 22:47:21 +0300

    Prepare application for Render deployment

--- config/database.js
+++ config/database.js
@@ -3,26 +3,40 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 
+
     try {
 
-        const connection = await mongoose.connect(process.env.MONGO_URI);
+
+        await mongoose.connect(
+
+            process.env.MONGO_URI
+
+        );
+
 
         console.log(
-            `MongoDB Connected: ${connection.connection.host}`
+            "MongoDB Connected Successfully"
         );
 
 
-    } catch (error) {
 
-        console.error(
-            `MongoDB Connection Error: ${error.message}`
+    } catch(error) {
+
+
+        console.log(
+            "MongoDB Connection Error:",
+            error.message
         );
+
 
         process.exit(1);
 
+
     }
 
+
 };
+
 
 
 module.exports = connectDB;
