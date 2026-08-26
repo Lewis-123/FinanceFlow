f4cd9f0599dd62e0f96bdb78d8bd3a2ac0629c50
Lewis-123 <mucherulewis@gmail.com>
2026-08-26 21:41:49 +0300

    Configure MongoDB database connection

--- app.js
+++ app.js
@@ -4,42 +4,73 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
+const connectDB = require('./config/database');
+
+// Connect to MongoDB
+connectDB();
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+
+// View engine setup
+var hbs = require('hbs');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-const hbs = require('hbs');
-
-hbs.registerPartials(__dirname + '/views/partials');
 
 
+// Register partials
+hbs.registerPartials(
+    path.join(__dirname, 'views/partials')
+);
+
+
+// Middleware
 app.use(logger('dev'));
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Routes
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
+// Catch 404 errors
 app.use(function(req, res, next) {
-  next(createError(404));
+
+    next(createError(404));
+
 });
 
-// error handler
+
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.locals.message = err.message;
+
+    res.locals.error = req.app.get('env') === 'development' 
+        ? err 
+        : {};
+
+    res.status(err.status || 500);
+
+    res.render('error');
+
 });
+
 
 module.exports = app;
