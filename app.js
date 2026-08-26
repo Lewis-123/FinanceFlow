e460ecaa5449961402848e01fe71e3cec7fd3ab7
Lewis-123 <mucherulewis@gmail.com>
2026-08-26 21:46:16 +0300

    Create user authentication foundation

--- app.js
+++ app.js
@@ -8,30 +8,72 @@ require('dotenv').config();
 
 const connectDB = require('./config/database');
 
-// Connect to MongoDB
 connectDB();
+
+
+const session = require("express-session");
+
+const MongoStore = require("connect-mongo");
+
+
+
+var hbs = require('hbs');
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
 
 // View engine setup
-var hbs = require('hbs');
 
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'hbs');
 
 
 // Register partials
+
 hbs.registerPartials(
     path.join(__dirname, 'views/partials')
 );
 
 
+
+// Session Configuration
+
+app.use(
+    session({
+
+        secret: process.env.SESSION_SECRET,
+
+        resave: false,
+
+        saveUninitialized: false,
+
+
+        store: MongoStore.create({
+
+            mongoUrl: process.env.MONGO_URI
+
+        }),
+
+
+        cookie: {
+
+            maxAge: 1000 * 60 * 60 * 24
+
+        }
+
+    })
+);
+
+
+
 // Middleware
+
 app.use(logger('dev'));
 
 app.use(express.json());
@@ -43,13 +85,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
 // Routes
+
 app.use('/', indexRouter);
 
 app.use('/users', usersRouter);
 
 
-// Catch 404 errors
+
+// Catch 404
+
 app.use(function(req, res, next) {
 
     next(createError(404));
@@ -57,13 +103,15 @@ app.use(function(req, res, next) {
 });
 
 
+
 // Error handler
+
 app.use(function(err, req, res, next) {
 
     res.locals.message = err.message;
 
-    res.locals.error = req.app.get('env') === 'development' 
-        ? err 
+    res.locals.error = req.app.get('env') === 'development'
+        ? err
         : {};
 
     res.status(err.status || 500);
